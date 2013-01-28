1f8687b10d3931b00932b903b953cf5ab0067ea7
Josh Wolfe <thejoshwolfe@gmail.com>
2013-01-28 02:10:20 -0700

    impementing between(). broken for between('z', '~11')

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 
-exports.zero = "0";
+var char_list = "0123456789?@ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+var radix = char_list.length;
+var values = {};
+(function() {
+  for (var i = 0; i < char_list.length; i++)
+    values[char_list[i]] = i;
+})();
+
+var zero = char_list[0];
+exports.zero = zero;
 
 exports.toNumber = function(n) {
-  var ref = splitAtDecimalPoint(n)
-    , integer = ref[0]
-    , fraction = ref[1]
-    ;
+  var integer = parse(n).integer;
   var result = 0;
   while (integer.length > 0) {
     result *= radix;
@@ -16,16 +22,13 @@ exports.toNumber = function(n) {
 };
 
 exports.next = function(n) {
-  var ref = splitAtDecimalPoint(n)
-    , integer = ref[0]
-    , fraction = ref[1]
-    ;
+  var integer = parse(n).integer;
   var result = "";
   var carry = 1;
   var orders = 0;
   for (var i = integer.length - 1; i >= 0 || carry > 0; i--) {
     orders++;
-    var value = i >= 0 ? values[integer[i]] : 0;
+    var value = values[integer[i] || zero];
     value += carry;
     carry = 0;
     while (value >= radix) {
@@ -35,30 +38,52 @@ exports.next = function(n) {
     result = char_list[value] + result;
   }
   for (i = 0; i < orders - 1; i++) {
-    result = order_signifier + result;
+    result = '~' + result;
   }
   return result;
 };
 
-exports.average = function(a, b) {
-  // TODO
+exports.between = function(a, b) {
+  if (a > b) throw new Error;
+  if (a === b) return a;
+  a = parse(a);
+  b = parse(b);
+  var a_digits = a.integer + a.fraction;
+  var b_digits = b.integer + b.fraction;
+  var i;
+  // pad the front of a with zeros to align the significance of the digitis
+  for (i = a.order.length; i < b.order.length; i++)
+    a_digits = zero + a_digits;
+  // pad the end of a with zeros so we can substr it later
+  while (a_digits.length < b_digits.length)
+    a_digits += zero;
+  var b_carry = 0;
+  for (i = 0; i < b_digits.length || b_carry > 0; i++) {
+    var a_value = values[a_digits[i] || zero];
+    var b_value = values[b_digits[i] || zero] + b_carry;
+    if (a_value === b_value) continue;
+    if (a_value === b_value - 1) {
+      // we need more digits, but remember that b is ahead
+      b_carry = radix;
+      continue;
+    }
+    // we have a distance of at least 2 between the values.
+    // choose a digit halfway between them.
+    var new_value = 0 | (a_value + b_value) / 2;
+    var result_digits = a_digits.substr(0, i) + char_list[new_value];
+    // strip away any zeros we added to the front
+    result_digits = result_digits.substr(b.order.length - a.order.length);
+    return a.order + result_digits;
+  }
+  throw new Error; // unreachable
 };
 
-function splitAtDecimalPoint(n) {
-  var orders = 1;
-  while (n.substr(0, 1) === order_signifier) {
-    orders++;
-    n = n.substr(1);
+function parse(n) {
+  var order = /^~*/.exec(n)[0];
+  return {
+    order:    order,
+    integer:  n.substr(order.length, order.length + 1),
+    fraction: n.substr(order.length + order.length + 1),
   }
-  return [n.substr(0, orders), n.substr(orders)];
 }
-
-var order_signifier = '~';
-var char_list = "0123456789?@ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-var radix = char_list.length;
-var values = {};
-(function() {
-  for (var i = 0; i < char_list.length; i++)
-    values[char_list[i]] = i;
-})();
 
--- test/test.js
+++ test/test.js
@@ -16,8 +16,14 @@ for (var i = 0; i < 10000; i++) {
 
 var zero = keese.zero;
 var one = keese.next(zero);
-var half = keese.average(zero, one);
+var half = keese.between(zero, one);
+console.log([zero, half, one]);
 assert(zero < half, "zero < half");
-assert(half < one, "half < one");
-assert.strictEqual(0.5, keese.toNumber(half));
+assert(half < one,  "half < one");
+assert(keese.toNumber(zero) < keese.toNumber(half), "numbers: zero < half");
+assert(keese.toNumber(half) < keese.toNumber(one),  "numbers: half < one");
+
+assert.throws(function() { keese.between(one,  zero); });
+assert.throws(function() { keese.between(one,  half); });
+assert.throws(function() { keese.between(half, zero); });
 
