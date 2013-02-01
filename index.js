4aa4d0f4741d554df1a41c8f6ec1842d09c4ffdd
Josh Wolfe <thejoshwolfe@gmail.com>
2013-02-01 01:29:26 -0700

    between works. test passes.

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 
 function closureGenerator(char_list) {
-  var keese = function(char_list) {
-    return closureGenerator(char_list);
-  };
   if (typeof char_list === "string") {
     if (char_list.length < 3) throw new Error("we need more chars");
   } else {
@@ -24,59 +21,76 @@ function closureGenerator(char_list) {
       values[char_list[i]] = i;
   })();
 
-  function parse(n) {
-    var order = n.substr(0, n.lastIndexOf(order_specifier) + 1);
+  function parse(s) {
+    var order_length = s.lastIndexOf(order_specifier) + 1;
     return {
-      order:    order,
-      integer:  n.substr(order.length, order.length + 1),
-      fraction: n.substr(order.length + order.length + 1),
+      order_length: order_length,
+      digits: s.substr(order_length)
     };
+  }
+  function construct(order_length, digits) {
+    var result = "";
+    for (var i = 0; i < order_length; i++)
+      result += order_specifier;
+    return result + digits;
+  }
+
+  function pad_to_equal_order(a, b) {
+    pad_in_place(a, b.order_length);
+    pad_in_place(b, a.order_length);
+  }
+  function pad_in_place(n, order_length) {
+    while (n.order_length < order_length) {
+      n.digits = zero + n.digits;
+      n.order_length++;
+    }
+  }
+
+  function add(a, b) {
+    pad_to_equal_order(a, b);
+    var result_digits = "";
+    var order_length = a.order_length;
+    var value = 0;
+    for (var i = Math.max(a.digits.length, b.digits.length) - 1; i >= 0; i--) {
+      value += values[a.digits[i] || zero];
+      value += values[b.digits[i] || zero];
+      result_digits = char_list[value % radix] + result_digits;
+      value = Math.floor(value / radix);
+    }
+    // overflow up to moar digits
+    while (value > 0) {
+      result_digits = char_list[value % radix] + result_digits;
+      value = Math.floor(value / radix);
+      order_length++;
+    }
+    return construct(order_length, result_digits);
   }
 
   // public exports
 
-  keese.zero = function() { return zero; };
-
-  keese.next = function(n) {
-    var integer = parse(n).integer;
-    var result = "";
-    var carry = 1;
-    var orders = 0;
-    for (var i = integer.length - 1; i >= 0 || carry > 0; i--) {
-      orders++;
-      var value = values[integer[i] || zero];
-      value += carry;
-      carry = 0;
-      while (value >= radix) {
-        value -= radix;
-        carry++;
-      }
-      result = char_list[value] + result;
-    }
-    for (i = 0; i < orders - 1; i++) {
-      result = order_specifier + result;
-    }
-    return result;
+  var keese = function(char_list) {
+    return closureGenerator(char_list);
   };
 
-  keese.between = function(a, b) {
-    if (a > b) throw new Error;
-    if (a === b) return a;
-    a = parse(a);
-    b = parse(b);
-    var a_digits = a.integer + a.fraction;
-    var b_digits = b.integer + b.fraction;
-    var i;
-    // pad the front of a with zeros to align the significance of the digitis
-    for (i = a.order.length; i < b.order.length; i++)
-      a_digits = zero + a_digits;
-    // pad the end of a with zeros so we can substr it later
-    while (a_digits.length < b_digits.length)
-      a_digits += zero;
+  keese.zero = function() { return zero; };
+
+  keese.next = function(s) {
+    var n = parse(s);
+    // drop the fraction
+    n.digits = n.digits.substr(0, n.order_length + 1);
+    return add(n, parse(char_list[1]));
+  };
+
+  keese.between = function(s1, s2) {
+    if (s1 > s2) throw new Error;
+    if (s1 === s2) return s1;
+    var a = parse(s1);
+    var b = parse(s2);
+    pad_to_equal_order(a, b);
     var b_carry = 0;
-    for (i = 0; i < b_digits.length || b_carry > 0; i++) {
-      var a_value = values[a_digits[i] || zero];
-      var b_value = values[b_digits[i] || zero] + b_carry;
+    for (var i = 0; i < a.digits.length || b_carry > 0; i++) {
+      var a_value = values[a.digits[i] || zero];
+      var b_value = values[b.digits[i] || zero] + b_carry;
       if (a_value === b_value) continue;
       if (a_value === b_value - 1) {
         // we need more digits, but remember that b is ahead
@@ -84,12 +98,16 @@ function closureGenerator(char_list) {
         continue;
       }
       // we have a distance of at least 2 between the values.
-      // choose a digit halfway between them.
-      var new_value = 0 | (a_value + b_value) / 2;
-      var result_digits = a_digits.substr(0, i) + char_list[new_value];
-      // strip away any zeros we added to the front
-      result_digits = result_digits.substr(b.order.length - a.order.length);
-      return a.order + result_digits;
+      // half the distance floored is sure to be a positive single digit.
+      var half_distance_value = Math.floor((b_value - a_value) / 2);
+      var half_distance_digits = "";
+      for (var j = 0; j < i; j++)
+        half_distance_digits += zero;
+      half_distance_digits += char_list[half_distance_value];
+      var half_distance = parse(construct(a.order_length, half_distance_digits));
+      // truncate insignificant digits of a
+      a.digits = a.digits.substr(0, i);
+      return add(a, half_distance);
     }
     throw new Error; // unreachable
   };
